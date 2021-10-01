15dc3501056fad029795b08587242a3187cc08ee
Mikhail Goncharov <goncharov.mikhail@gmail.com>
2021-10-01 23:09:18 +0200

    type / formatting

--- twitch_auth.py
+++ twitch_auth.py
@@ -184,18 +184,22 @@ class UserAuthenticator:
             response = requests.post(url)
             data: dict = response.json()
             logging.info(f'{data}')
-            self.callback_func(data['access_token'], data['refresh_token'])
+            if callback_func:
+                callback_func(data['access_token'], data['refresh_token'])
 
-logging.basicConfig(stream= sys.stdout,
-                    format= '%(asctime)s %(levelname)s %(message)s',
+
+logging.basicConfig(stream=sys.stdout,
+                    format='%(asctime)s %(levelname)s %(message)s',
                     level=logging.INFO)
 
 APP_ID = os.getenv('TWITCH_API_APP_ID')
 APP_SECRET = os.getenv('TWITCH_API_APP_SECRET')
 AUTH_URL = os.getenv('TWITCH_API_AUTH_URL', 'http://localhost:17563')
 
+
 def auth_callback(*args):
     logging.info(f'auth callback {args}')
+
 
 twitch = Twitch(APP_ID, APP_SECRET)
 target_scope = [AuthScope.CHANNEL_READ_REDEMPTIONS]
