c52ff1b6e9135d8a34e37119c753494bcc74ef8c
Mikhail Goncharov <goncharov.mikhail@gmail.com>
2022-08-16 21:58:17 +0200

    restart script

--- restart.sh
+++ restart.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 pkill -f /root/.local/share/virtualenvs/moon-rabbit-T0OZLLxu/bin/python3
+sleep 5s
+cd /var/moon-rabbit
+echo $(date) > restart_date.txt
 nohup pipenv run python3 main.py --twitch bo_ted --log bo_ted > nohup1.out 2>&1 &
 nohup pipenv run python3 main.py --discord --log_level INFO --log discord > nohup2.out 2>&1 &
 nohup pipenv run python3 main.py --twitch moon_robot --log_level INFO --log moon_robot > nohup2.out 2>&1 &
